59cd3132a35a851def413c315c68a4dcb3b4053b
sanjeetpal1997 <palsanjeet1997@gmail.com>
2020-11-03 13:03:53 +0530

    Added lazy load

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import './App.css';
-import CovidMainContainer from './components/covid/CovidMainContainer'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import Logout from './components/auth/Logout'
+import React, { Suspense } from "react";
+import "./App.css";
+import CovidMainContainer from "./components/covid/CovidMainContainer";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Logout from "./components/auth/Logout";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Login from './components/auth/Login'
-import Registration from './components/auth/Registration'
-import Header from './components/header/Header'
-import TodoMainContainer from './components/todo/TodoMainContainer';
+import Login from "./components/auth/Login";
+import Registration from "./components/auth/Registration";
+import Header from "./components/header/Header";
+
+const TodoMainContainer = React.lazy(() =>
+  import("./components/todo/TodoMainContainer")
+);
 
 function App() {
   return (
@@ -24,23 +23,25 @@ function App() {
           <Header />
           <div className="main">
             <div className="wrapper">
-              <Switch>
-                <Route path="/login">
-                  <Login />
-                </Route>
-                <Route path="/register">
-                  <Registration />
-                </Route>
-                <Route exact path="/logout">
-                  <Logout />
-                </Route>
-                <Route path="/todo">
-                  <TodoMainContainer />
-                </Route>
-                <Route path="/">
-                  <CovidMainContainer />
-                </Route>
-              </Switch>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                  <Route path="/login">
+                    <Login />
+                  </Route>
+                  <Route path="/register">
+                    <Registration />
+                  </Route>
+                  <Route exact path="/logout">
+                    <Logout />
+                  </Route>
+                  <Route path="/todo">
+                    <TodoMainContainer />
+                  </Route>
+                  <Route path="/">
+                    <CovidMainContainer />
+                  </Route>
+                </Switch>
+              </Suspense>
             </div>
           </div>
         </div>
